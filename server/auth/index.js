889d891bc4bae4234790840fb28ec965c062233c
yasser sabsibo <yassersabsibo@gmail.com>
2022-06-29 17:13:26 +0100

    create signup end point

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const joi = require('joi');
+const db = require('../db/connection.js');
+const bcrypt=require('bcryptjs');
+
+
+const users = db.get('users');
+
 const router = express.Router()
+
+// for fast querying database
+// users.index('username');
+users.createIndex('username',{unique:true});
+
+const schema =  joi.object().keys({
+    username:joi.string().regex(/^[a-zA-Z0-9_]+$/).min(3).max(30).required(),
+    password:joi.string().min(6).trim().required()
+});
 
 router.get('/',(req,res)=>{
     res.json({
@@ -7,11 +23,34 @@ router.get('/',(req,res)=>{
     });
 });
 
-router.post('/signup',(req,res)=>{
-    console.log('body',req.body);
-    res.json({
-        message:'you are in signup page !'
-    });
+router.post('/signup',(req,res,next)=>{
+    const result = schema.validate(req.body);
+    if(result.error==null){
+        users.findOne({
+            username:req.body.username
+        }).then((user)=>{
+            if(user){
+                const error=new Error('user already exist !!');
+                next(error);
+            }else{
+                bcrypt.hash(req.body.password,12).then(hashed=>{
+                    const newUser = {
+                        username:req.body.username,
+                        password:hashed
+                    };
+                    users.insert(newUser).then(inserted=>{
+                        delete inserted.password;
+                        res.json(inserted);
+                    });
+                });
+            }
+        });
+    }else{
+        next(res.error);
+        // res.json({
+        //     result
+        // });
+    }
 });
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,21 +1,41 @@
 const express = require('express');
 const volleyball = require('volleyball');
-const auth = require('./auth/index.js');
-const bodyParser = require('body-parser');
+// if the file inside directory called index then you can just called the dirrectory that contain it
+// const auth = require('./auth/index.js');
+const auth = require('./auth');
 
 const app = express();
 
 app.use(volleyball);
-app.use(bodyParser);
 
-// app.get('/',(req,res)=>{
-//     res.json({
-//         message:'hello world !'
-//     });
-// });
+// build in body parser
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.json({
+        message:'hello world !'
+    });
+});
 
 // auth gonna be prepend by auth
 app.use('/auth',auth);
+
+function notFound(req,res,next){
+    res.status(404);
+    const error=new Error('not found'+req.originalUrl);
+    next(error);
+}
+
+function errorHandler(err,req,res,next){
+    res.status(res.statusCode||500);
+    res.json({
+        message:err.message,
+        stack:err.stack
+    });
+}
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen('5000',()=>{
     console.log('you are logged in yasser!');
